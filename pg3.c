2156f77fbac0b8be31e1819a8a15941e035c74ed
vinayaka593 <153350061+vinayaka593@users.noreply.github.com>
2024-02-20 15:55:52 +0530

    Update pg3.c

--- pg3.c
+++ pg3.c
@@ -76,30 +76,3 @@ void myStackFree(MyStack* q) {
     free(q);
 }
 
-/**
- * Your MyStack struct will be instantiated and called as such:
- * MyStack* obj = myStackCreate();
- * myStackPush(obj, x);
- 
- * int param_2 = myStackPop(obj);
- 
- * int param_3 = myStackTop(obj);
- 
- * bool param_4 = myStackEmpty(obj);
- 
- * myStackFree(obj);
-*/
-
-/**
- * Your MyStack struct will be instantiated and called as such:
- * MyStack* obj = myStackCreate();
- * myStackPush(obj, x);
- 
- * int param_2 = myStackPop(obj);
- 
- * int param_3 = myStackTop(obj);
- 
- * bool param_4 = myStackEmpty(obj);
- 
- * myStackFree(obj);
-*/
